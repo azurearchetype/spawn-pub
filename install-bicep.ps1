322f55e67883a71402a4402b94fef280c1f1fcf0
azurearchetype <matthew@azurearchetype.com>
2024-09-28 16:50:54 -0700

    Update install-bicep.ps1

--- install-bicep.ps1
+++ install-bicep.ps1
@@ -1,27 +1,27 @@
-# Define the directory where Azure CLI will be installed
-$installDir = "C:\Bicep"
+# Define the directory where Bicep will be installed
+$installDir2 = "C:\Bicep"
 
 # Create the directory if it doesn't exist
-if (-Not (Test-Path -Path $installDir)) {
-    New-Item -ItemType Directory -Path $installDir
+if (-Not (Test-Path -Path $installDir2)) {
+    New-Item -ItemType Directory -Path $installDir2
 }
 
 # Define the URL for the Azure CLI installer
-$installerUrl = "https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe"
+$installerUrl2 = "https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe"
 
 # Define the path for the installer
-$installerPath = "$installDir\bicep-win-x64.exe"
+$installerPath2 = "$installDir2\bicep-win-x64.exe"
 
 # Download the installer
-Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+Invoke-WebRequest -Uri $installerUrl2 -OutFile $installerPath2
 
 # Install Azure CLI
-Start-Process -FilePath msiexec.exe -ArgumentList "/i $installerPath /quiet" -Wait
+Start-Process -FilePath msiexec.exe -ArgumentList "/i $installerPath2 /quiet" -Wait
 
 # Clean up the installer file
 # Remove-Item -Path $installerPath
 
-Write-Output "Bicep Extensions have been installed successfully in $installDir"
+Write-Output "Bicep Extensions have been installed successfully in $installDir2"
 
 
 # create local directory

a8cbb7044a775d17e32a9e7b0edbba62436258c7
azurearchetype <matthew@azurearchetype.com>
2024-09-30 20:28:07 -0700

    Update createParamFile.ps1

--- createParamFile.ps1
+++ createParamFile.ps1
@@ -1,3 +1,10 @@
+#Define Variables to be written to the parameter file
+param (
+    [string]$vmName,
+    [string]$region,
+    [string]$appResourceGroup
+    )
+
 # Define the root directory from where the Azure Marketplace offer will be deployed
 $CreateDirAzmOffer = "C:\azmOffer"
 
@@ -6,12 +13,6 @@ if (-Not (Test-Path -Path $CreateDirAzmOffer)) {
     New-Item -ItemType Directory -Path $CreateDirAzmOffer
 }
 
-#Define Variables to be written to the parameter file
-param (
-    [string]$vmName,
-    [string]$region,
-    [string]$appResourceGroup
-    )
 $json = @{
         '$schema' ='https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#'
             contentVersion = '1.0.0.0'

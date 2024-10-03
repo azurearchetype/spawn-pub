eff9d089adcbb09dbd71e8130d12d38014366977
azurearchetype <matthew@azurearchetype.com>
2024-10-03 13:43:33 -0700

    Update createParamFile.ps1

--- createParamFile.ps1
+++ createParamFile.ps1
@@ -16,9 +16,28 @@ if (-Not (Test-Path -Path $CreateDirAzmOffer)) {
 $resourceGroup = "spawn"
 $deploymentName = "mainTemplate"
 
-# Retrieve the outputs
-Start-Process powershell -ArgumentList "-Command", "$outputs = az deployment group show --resource-group $resourceGroup --name $deploymentName --query properties.outputs"
+# Construct the command string
+$command = "az deployment group show --resource-group $resourceGroup --name $deploymentName --query properties.outputs"
 
+# Execute the command using Invoke-Expression and capture the output
+$outputsJson = Invoke-Expression $command
 
-# Write outputs to a file on the VM
-$outputs | Out-File -FilePath "$CreateDirAzmOffer\parameters.json"
+# Convert the JSON output to a PowerShell object
+$outputs = $outputsJson | ConvertFrom-Json
+
+# Prepare the parameters file structure
+$parameters = @{
+    "$schema" = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
+    "contentVersion" = "1.0.0.0"
+    "parameters" = @{}
+}
+
+# Populate the parameters section based on the outputs
+foreach ($output in $outputs.PSObject.Properties) {
+    $parameters.parameters[$output.Name] = @{
+        "value" = $output.Value
+    }
+}
+
+# Write outputs to a file on the VM in Bicep parameters file format
+$parameters | ConvertTo-Json -Depth 10 | Out-File -FilePath "$CreateDirAzmOffer\parameters.json"
